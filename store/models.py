d0571fc0eca897ce11dd1f9c5f3ca283bfbd7768
karabekov012-ship-it <karabekov012@gmail.com>
2025-10-03 16:28:21 +0600

    1

---
+++ store/models.py
@@ -0,0 +1,62 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from phonenumber_field.modelfields import PhoneNumberField
+
+class UserProfile(AbstractUser):
+    age = models.PositiveSmallIntegerField(validators=[MinValueValidator(15),
+                                                       MaxValueValidator(70)],
+                                           null=True, blank=True)
+    phone_number = PhoneNumberField()
+    STATUS_CHOICE = (
+        ('gold', 'gold'), #75%
+        ('silver', 'silver'), #50%
+        ('bronze', 'bronze'), #25%
+        ('simple', 'simple') #0%
+    )
+    status = models.CharField(max_length=32, choices=STATUS_CHOICE, default='simple')
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.first_name}-{self.last_name}'
+
+class Category(models.Model):
+    category_image = models.ImageField(upload_to="category_image/")
+    category_name = models.CharField(max_length=64, unique=True)
+
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subcategory_name = models.CharField(max_length=64, unique=True)
+
+
+class Product(models.Model):
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=56)
+    price = models.PositiveSmallIntegerField()
+    article_number = models.PositiveSmallIntegerField(unique=True)
+    descriptions = models.TextField()
+    image = models.ImageField(upload_to='image/')
+    video = models.FileField(upload_to='video/', null=True, blank=True)
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='image/')
+
+
+class Review(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    comment = models.TextField()
+    stars = models.CharField(choices=[(i, str(1)) for i in range(1,6)])
+    created_date = models.DateField(auto_now_add=True)
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=1)
